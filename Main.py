752f2dc5b3c23e61cffb50b461aa92853c00478f
SergioASU <sicorral@asu.edu>
2019-06-25 20:14:56 -0700

    Added email to Main

--- Main.py
+++ Main.py
@@ -1,3 +1,15 @@
+import smtplib, ssl
+
 def main():
     print("Auto-Cat starting up")
+    port = 465  # For SSL
+    password = 'AutoCatProject'
+
+    # Create a secure SSL context
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
+        server.login("Auto.Cat.Messaging@gmail.com", password)
+        server.sendmail('Auto.Cat.Messaging@gmail.com', 'sergio.c.842@gmail.com', 'test')
+        server.quit() 
 main()
--- SMS.py
+++ SMS.py
@@ -1,21 +1,12 @@
-import smtplib
+import smtplib, ssl
 
-class SMS:
-    carriers = {
-	'att':    '@mms.att.net',
-    'tmobile':' @tmomail.net',
-	'verizon':  '@vtext.com',
-	'sprint':   '@page.nextel.com'
-    }
-    def __init__(self, carrier, number):
-        self.carrier = carrier
-        self.number = number
-    def send(self,message):
-        to_number = self.number.format(self.carriers[self.carrier])
-        auth = ('**email**', '**password**')
-        #Establish a secure session with gmail's outgoing SMTP server using your gmail account
-        server = smtplib.SMTP( "smtp.gmail.com", 587 )
-        server.starttls()
-        server.login(auth[0], auth[1])
-	    # Send text message through SMS gateway of destination number
-        server.sendmail(auth[0],to_number,message)
+port = 465  # For SSL
+password = 'AutoCatProject'
+
+# Create a secure SSL context
+context = ssl.create_default_context()
+
+with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
+    server.login("Auto.Cat.Messaging@gmail.com", password)
+    server.sendmail('Auto.Cat.Messaging@gmail.com', 'sergio.c.842@gmail.com', 'test')
+    server.quit() 
