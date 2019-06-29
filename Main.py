501880a3f5223d80a99cb9697469a45aea65a9e0
Sergio Corral <sicorral@asu.edu>
2019-06-29 00:12:07 -0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,16 +1,27 @@
 from Email import *
+from Motor import *
 import time
 import schedule
 
-def main():
+def initialization():
     print("Auto-Cat initializing")
     email = Email(465,'Auto.Cat.Messaging@gmail.com','AutoCatProject')
-    schedule.every(1).minutes.do(email.sendEmail,'sergio.c.842@gmail.com','This is a test')
+    motor = Motor(17,50,5)
+    #schedule.every(1).minutes.do(email.sendEmail,'sergio.c.842@gmail.com','This is a test')
+    schedule.every().day.at("22:19").do(email.sendEmail,'eliazar.d.842@gmail.com','hey :)')
+    schedule.every().day.at("22:44").do(motor.feed,10,5)
+    #Add any other initializing components here
     print("Auto-Cat initializing complete")
-    
+
+def main():
+    #Call initilization function
+    initialization()
+
+    #loop forever
     while True:
         print('Auto-Cat running')
         schedule.run_pending()
         time.sleep(1)
     
+#Call main function
 main()
