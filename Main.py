59bb2c2e1a5b11433987979fa30d5c4f7099442b
Sergio <sicorral@asu.edu>
2019-07-07 18:53:33 -0700

    Added GPIO functionality

--- Main.py
+++ Main.py
@@ -6,10 +6,10 @@ import schedule
 def initialization():
     print("Auto-Cat initializing")
     email = Email(465,'Auto.Cat.Messaging@gmail.com','AutoCatProject')
-    motor = Motor(17,50,5)
+    motor = Motor(21,50,5)
     #schedule.every(1).minutes.do(email.sendEmail,'sergio.c.842@gmail.com','This is a test')
-    schedule.every().day.at("22:19").do(email.sendEmail,'eliazar.d.842@gmail.com','hey :)')
-    schedule.every().day.at("22:44").do(motor.feed,10,5)
+    #schedule.every().day.at("22:19").do(email.sendEmail,'eliazar.d.842@gmail.com','hey :)')
+    schedule.every(1).minutes.do(motor.feed,10,5)
     #Add any other initializing components here
     print("Auto-Cat initializing complete")
 
--- Motor.py
+++ Motor.py
@@ -1,22 +1,31 @@
 import RPi.GPIO as GPIO
 import time
+from Email import *
+import datetime
+
 class Motor:
     #its just how it goes
     def __init__(self, servoPin, htz,startAngle):
+        self.email = Email(465,'Auto.Cat.Messaging@gmail.com','AutoCatProject')
+        self.htz = htz
         self.servoPin = servoPin
-        self.GPIO.setmode(GPIO.BCM)
-        self.GPIO.setup(self.servoPIN, GPIO.OUT)
-        self.p = GPIO.PWM(self.servoPIN, self.htz)
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.servoPin, GPIO.OUT)
+        self.p = GPIO.PWM(self.servoPin, self.htz)
         self.startAngle = startAngle
         self.p.ChangeDutyCycle(self.startAngle)
     def feed(self,toAngle,timeToRun):
         print("Feeding now")
         self.p.ChangeDutyCycle(toAngle)
         time.sleep(timeToRun)
-        self.ChangeDutyCycle(self.startAngle)
+        self.p.ChangeDutyCycle(self.startAngle)
         time.sleep(0.5)
+        feedingMessage = 'Feeding complete on: ' + str(datetime.datetime.now())
+        print(feedingMessage)
+        self.email.sendEmail('sergio.c.842@gmail.com','Feeding Complete!')
+        self.email.sendEmail('martin.c.842@gmail.com', 'Feeding Complete!')
         print("Feeding Complete")
 
     def end(self):
         self.p.stop()
-        self.GPIO.cleanup()
+        GPIO.cleanup()
